e23fd4292b9dfb3dff26f57b4f160fe4578d8ea9
Lev Pavelko <neroheroes6@gmail.com>
2023-01-06 22:11:55 +0200

    Deleted unnecessary code for now, refactoring of code, redo jumping code

--- Assets/Scripts/MainRayCast.cs
+++ Assets/Scripts/MainRayCast.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class MainRayCast : MonoBehaviour
 {
   private Ray ray;
-  //[SerializeField] private float distance;
   public Ray Ray { get { return ray; } set { ray = value; } }
 
 
   private void Update()
   {
     Ray = new Ray(transform.position, transform.forward);
-    //Debug.DrawRay(transform.position, transform.forward * distance, Color.magenta);
   }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,111 +1,92 @@
+using System.Collections.Generic;
 using UnityEngine;
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
   private Vector3 PlayerMovementInput;
   private Vector2 PlayerMouseInput;
   private Vector3 MoveVector;
   private Camera main;
   private GameObject player;
+  private Collider playerCollider;
   private Rigidbody rb;
-  private Transform feet;
   [SerializeField] private Transform rArm;
-  [SerializeField] private LayerMask FloorMask;
-  [SerializeField] private LayerMask PropLayer;
-  int currlayerMask;
   [SerializeField] private float speed = 5f;
   [SerializeField] private float jumpForce = 10f;
-  [SerializeField] private float climbForce = 10f;
   [SerializeField] private float sensitivity = 1f;
-  [SerializeField] private bool onLadder = false;
+  [SerializeField] private bool canJump;
+  [SerializeField] private bool crouching;
+  [SerializeField] private bool grounded;
   private float rotX;
   private float rotY;
   private Vector3 plLocScale;
-  void Start()
-  {
+  Dictionary<string, float> pData;
+  void Start() {
     Cursor.lockState = CursorLockMode.Locked;
-
     player = transform.gameObject;
     rb = player.GetComponent<Rigidbody>();
-    feet = player.transform.GetChild(1);
+    playerCollider = player.GetComponent<Collider>();
     main = Camera.main;
     plLocScale = player.transform.localScale;
-    currlayerMask = FloorMask | PropLayer;
+    pData = new() {
+      ["scaleY"] = player.transform.localScale.y,
+      ["radius"] = (player.transform.localScale.x / 2f) - 0.1f,
+    };
   }
 
-  void Update()
-  {
+  void Update() {
     PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
     PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    grounded = isGrounded(player, playerCollider);
     Move();
     MoveCamera();
   }
 
-  void MoveCamera()
-  {
+  void MoveCamera() {
     rotX -= PlayerMouseInput.y * sensitivity * Time.deltaTime;
     rotX = Mathf.Clamp(rotX, -90f, 90f);
 
     rotY = PlayerMouseInput.x * sensitivity * Time.deltaTime;
-    
+
     player.transform.Rotate(0f, rotY, 0f);
     main.transform.localRotation = Quaternion.Euler(rotX, 0f, 0f);
     rArm.transform.localRotation = main.transform.localRotation;
   }
 
-  private void Move()
-  {
+  private void Move() {
     MoveVector = transform.TransformDirection(PlayerMovementInput) * speed;
     rb.velocity = new Vector3(MoveVector.x, rb.velocity.y, MoveVector.z);
-    if (Input.GetKeyDown(KeyCode.Space) && Physics.CheckSphere(feet.position, 0.1f, currlayerMask))
-    {
-      if (!checkCeil(feet, 1.75f, 0.35f) && onLadder == false) Jump();
-    }
+
+    if (Input.GetKeyDown(KeyCode.Space) && grounded) canJump = true;
+
     if (Input.GetKey(KeyCode.LeftControl))
-    {
-      player.transform.localScale = new Vector3(plLocScale.x, plLocScale.y * 0.75f, plLocScale.z);
+      crouching = true;
+
+    if (crouching && player.transform.localScale.y == pData["scaleY"]) {
+      player.transform.localScale = new Vector3(plLocScale.x, 0.5f, plLocScale.z);
     }
-    if (Input.GetKeyUp(KeyCode.LeftControl) || !Input.GetKey(KeyCode.LeftControl))
-    {
-      if (!checkCeil(feet, 1.5f, 0.4f))
-      {
-        player.transform.localScale = plLocScale;
+
+    if (!Input.GetKey(KeyCode.LeftControl) && crouching)
+      if (!SphereChecker(player.transform, pData["scaleY"], pData["radius"])) {
+        crouching = false;
       }
-    }
+
+    if (!crouching)
+      player.transform.localScale = new Vector3(plLocScale.x, pData["scaleY"], plLocScale.z);
   }
 
-  bool checkCeil(Transform pos1, float yExpand, float radius)
-  {
-    return Physics.CheckSphere(pos1.position + new Vector3(0f, yExpand, 0f), radius, currlayerMask);
-  }
+  bool SphereChecker(Transform pos1, float yExpand, float radius) =>
+    Physics.CheckSphere(pos1.position + new Vector3(0f, yExpand, 0f), radius);
 
+  bool isGrounded(GameObject obj, Collider collider) =>
+    Physics.Raycast(obj.transform.position, -obj.transform.up, collider.bounds.extents.y + 0.1f);
 
-  void Jump()
-  {
-    rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
-  }
+  void Jump() => rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
 
   private void FixedUpdate()
   {
-    if (onLadder == true && Input.GetKey(KeyCode.Q))
-    {
-      rb.AddForce(0, climbForce, 0, ForceMode.Force);
+    if (canJump) {
+      Jump();
+      canJump = false;
     }
-  }
 
-  private void OnTriggerEnter(Collider collider)
-  {
-    if (collider.gameObject.GetComponent<LadderComponent>() != null)
-    {
-      onLadder = true;
-      //rb.useGravity = false;
-    }
-  }
-  private void OnTriggerExit(Collider collider)
-  {
-    if (collider.gameObject.GetComponentInChildren<LadderComponent>() != null)
-    {
-      onLadder = false;
-      //rb.useGravity = true;
-    }
   }
 }
